3c430e72d520458ad7e0bd69814953cf2fa83736
pwhitdog <paul.whitmer@gmail.com>
2016-06-24 14:49:10 -0700

    fixing create bootscript part deux

--- gruntConfigs/grunt-launchInstance.js
+++ gruntConfigs/grunt-launchInstance.js
@@ -4,7 +4,7 @@ var child = require('child_process');
 function launchInstance(appName, profile, instanceProfileName, securityGroup) {
     var count = 2;
 
-    var filename = "file://../socalcodecampdemo/boot" + appName + ".sh";
+    var filename = "file://../boot" + appName + ".sh";
     return function() {
         var command = 'aws ec2 run-instances --profile ' + profile + ' --iam-instance-profile Name=' + instanceProfileName + ' --image-id ami-be14d3de --user-data ' +
             filename + '  --count ' + count + ' --instance-type t2.small --security-group-ids ' + securityGroup +  ' --key-name eng2-ssh --associate-public-ip-address';
