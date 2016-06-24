7a2e432b19f194c0c46ddd06c67edaa9fe82b652
pwhitdog <paul.whitmer@gmail.com>
2016-06-24 15:17:45 -0700

    Removing iamProfile

--- gruntConfigs/grunt-launchInstance.js
+++ gruntConfigs/grunt-launchInstance.js
@@ -6,7 +6,7 @@ function launchInstance(appName, profile, instanceProfileName, securityGroup) {
 
     var filename = "file://../boot" + appName + ".sh";
     return function() {
-        var command = 'aws ec2 run-instances --profile ' + profile + ' --iam-instance-profile Name=' + instanceProfileName + ' --image-id ami-be14d3de --user-data ' +
+        var command = 'aws ec2 run-instances --profile ' + profile + ' --image-id ami-be14d3de --user-data ' +
             filename + '  --count ' + count + ' --instance-type t2.small --security-group-ids ' + securityGroup +  ' --key-name eng2-ssh --associate-public-ip-address';
 
         child.execSync(command);
