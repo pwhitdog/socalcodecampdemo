cdb612eaa03874c041ed372996e1ddfb1900a99e
pwhitdog <paul.whitmer@gmail.com>
2016-06-24 14:20:12 -0700

    fixing create bootscript

--- gruntConfigs/grunt-createBootScript.js
+++ gruntConfigs/grunt-createBootScript.js
@@ -11,7 +11,7 @@ function createBootScript(appName, bucketName, lbName) {
         var fileContents = fs.readFileSync( templatePath, { encoding: "utf-8" });
         fileContents = fileContents.replace('$1', lbName).replace('$2', bucketName).replace('$3', appName);
 
-        fs.writeFileSync('../socalcodecampdemo/boot' + appName + '.sh', fileContents);
+        fs.writeFileSync('../boot' + appName + '.sh', fileContents);
         console.log('File created.');
     }
 }
