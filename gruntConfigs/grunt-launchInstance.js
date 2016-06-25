55657f8fb299a3ecc4598171fc5e123f02b3af69
pwhitdog <paul.whitmer@gmail.com>
2016-06-25 10:10:54 -0700

    new ami

--- gruntConfigs/grunt-launchInstance.js
+++ gruntConfigs/grunt-launchInstance.js
@@ -6,7 +6,7 @@ function launchInstance(appName, profile, instanceProfileName, securityGroup) {
 
     var filename = "file://../boot" + appName + ".sh";
     return function() {
-        var command = 'aws ec2 run-instances --profile ' + profile + ' --image-id ami-3f56915f --user-data ' +
+        var command = 'aws ec2 run-instances --profile ' + profile + ' --image-id ami-5e54933e --user-data ' +
 
             filename + '  --count ' + count + ' --instance-type t2.small --security-group-ids ' + securityGroup +  ' --key-name aws-key --associate-public-ip-address';
 
