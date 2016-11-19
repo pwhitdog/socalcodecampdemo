e729bf4f96c39448849707e9067fb916094c1ed2
Jim Counts <jcounts18@gmail.com>
2016-11-19 07:46:39 -0800

    Cleanup Grunt

--- gruntConfigs/grunt-launchInstance.js
+++ gruntConfigs/grunt-launchInstance.js
@@ -1,18 +1,20 @@
-'use strict'
+'use strict';
 var child = require('child_process');
 
-function launchInstance(profile, instanceProfileName, securityGroup, yourAMIGoesHERE, params) {
+module.exports = function (params) {
     var count = 2;
     var sshKeyName = 'demos-ci';
-
     var filename = "file://../boot" + params.appName + ".sh";
-    return function() {
-        var command = 'aws ec2 run-instances --image-id ' + params.ami + ' --user-data ' +
+    var command = 'aws ec2 run-instances --image-id ' + params.ami +
+        ' --user-data ' + filename +
+        ' --count ' + count +
+        ' --instance-type t1.micro ' +
+        ' --security-group-ids ' + params.securityGroup +
+        ' --key-name ' + sshKeyName +
+        ' --associate-public-ip-address';
 
-            filename + '  --count ' + count + ' --instance-type t2.small --security-group-ids ' + params.securityGroup +  ' --key-name ' + sshKeyName + ' --associate-public-ip-address';
-
+    return function () {
         child.execSync(command);
     }
-}
+};
 
-module.exports = launchInstance;
