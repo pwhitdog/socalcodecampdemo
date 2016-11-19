40c9ca3b72a8a25313b009331575a342e3c7897d
Jim Counts <jcounts18@gmail.com>
2016-11-19 07:07:20 -0800

    Updated ssh key name

--- gruntConfigs/grunt-launchInstance.js
+++ gruntConfigs/grunt-launchInstance.js
@@ -3,12 +3,13 @@ var child = require('child_process');
 
 function launchInstance(profile, instanceProfileName, securityGroup, yourAMIGoesHERE, params) {
     var count = 2;
+    var sshKeyName = 'demos-ci';
 
     var filename = "file://../boot" + params.appName + ".sh";
     return function() {
         var command = 'aws ec2 run-instances --image-id ' + params.ami + ' --user-data ' +
 
-            filename + '  --count ' + count + ' --instance-type t2.small --security-group-ids ' + params.securityGroup +  ' --key-name aws-key --associate-public-ip-address';
+            filename + '  --count ' + count + ' --instance-type t2.small --security-group-ids ' + params.securityGroup +  ' --key-name ' + sshKeyName + ' --associate-public-ip-address';
 
         child.execSync(command);
     }
