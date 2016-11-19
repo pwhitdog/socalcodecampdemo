45a0c5315e8d7065cffcdf5e03ca22c89c544c8c
Jim Counts <jcounts18@gmail.com>
2016-11-19 07:47:11 -0800

    Remove profile arg from tagging

--- gruntConfigs/bootGenericServerTemplate.sh
+++ gruntConfigs/bootGenericServerTemplate.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 LB=$1
-BUCKETNAME=$2
-APPNAME=$3
+BUCKET_NAME=$2
+APP_NAME=$3
 
-echo 'LB: ' $LB
-echo 'Bucket Name: ' $BUCKETNAME
-echo 'Appname: ' $APPNAME
+echo 'LB: ' ${LB}
+echo 'Bucket Name: ' ${BUCKET_NAME}
+echo 'App Name: ' ${APP_NAME}
 
 INSTANCE=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
-echo "instance name = " $INSTANCE  >> /home/ec2-user/instance.log
-sudo su -c "aws ec2 create-tags --resources $INSTANCE --tags Key=Name,Value=$APPNAME --region us-west-2 --profile demo" ec2-user
+echo "instance name = " ${INSTANCE}  >> /home/ec2-user/instance.log
+echo "Tagging ${INSTANCE} with App Name"
+sudo su -c "aws ec2 create-tags --resources $INSTANCE --tags Key=Name,Value=$APP_NAME --region us-west-2" ec2-user
 
 echo "registering with elastic load balancer" >> /home/ec2-user/instance.log
 sudo su -c "aws elb register-instances-with-load-balancer --load-balancer-name $LB --instances $INSTANCE --region us-west-2 --profile demo" ec2-user
@@ -17,10 +18,10 @@ echo "load balanced" >> /home/ec2-user/instance.log
 
 cd /home/ec2-user
 echo "starting getLatestDeploy"  >> /home/ec2-user/instance.log
-sudo su -c "~/.nvm/versions/node/v6.2.2/bin/node /home/ec2-user/getLatestDeploy.js $APPNAME $BUCKETNAME" ec2-user
+sudo su -c "~/.nvm/versions/node/v6.2.2/bin/node /home/ec2-user/getLatestDeploy.js $APP_NAME $BUCKET_NAME" ec2-user
 VERSION=`ls *.zip`
 sudo su -c "aws ec2 create-tags --resources $INSTANCE --tags Key=Version,Value=$VERSION --region us-west-2 --profile demo" ec2-user
-cd /home/ec2-user/$APPNAME
+cd /home/ec2-user/${APP_NAME}
 echo "starting app" >> /home/ec2-user/instance.log
 echo ${LB} ${INSTANCE} ${VERSION} >> /home/ec2-user/instance.log
 
