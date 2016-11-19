ecea358d7829fccd796c854156199d205b3cd3ac
Jim Counts <jcounts18@gmail.com>
2016-11-19 08:20:30 -0800

    Fixed instance type

--- gruntConfigs/grunt-launchInstance.js
+++ gruntConfigs/grunt-launchInstance.js
@@ -5,10 +5,12 @@ module.exports = function (params) {
     var count = 2;
     var sshKeyName = 'demos-ci';
     var filename = "file://../boot" + params.appName + ".sh";
+    var instanceType = 't2.micro';
+
     var command = 'aws ec2 run-instances --image-id ' + params.ami +
         ' --user-data ' + filename +
         ' --count ' + count +
-        ' --instance-type t1.micro ' +
+        ' --instance-type ' + instanceType +
         ' --security-group-ids ' + params.securityGroup +
         ' --key-name ' + sshKeyName +
         ' --associate-public-ip-address';
