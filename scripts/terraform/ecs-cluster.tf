d654f007003190930fd7e651b469d7353f80e453
Jim Counts <jcounts18@gmail.com>
2016-11-22 09:00:14 -0800

    Initial terraform setup

---
+++ scripts/terraform/ecs-cluster.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0a5df16a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-724c140b"]
+}
