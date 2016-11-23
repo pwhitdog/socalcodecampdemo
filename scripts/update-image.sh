58db95f5d8b27feefda565d18b4c26e250edc2d9
Jim Counts <jcounts18@gmail.com>
2016-11-23 05:31:25 -0800

    Commentary

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Logging into AWS ECR"
 aws ecr get-login | bash
+
+echo "Building image ${REPOSITORY_NAME}"
 docker build -t ${REPOSITORY_NAME} .
+
+echo "Tagging ${REPOSITORY_NAME} for ${REPOSITORY_URL}"
 docker tag ${REPOSITORY_NAME}:latest \
     ${REPOSITORY_URL}/${REPOSITORY_NAME}:latest
+
+echo "Pushing image to ECR"
 docker push ${REPOSITORY_URL}/${REPOSITORY_NAME}:latest
