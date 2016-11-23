353778ffe8089f3fc7cc42611239e95be44f03b7
Jim Counts <jcounts18@gmail.com>
2016-11-23 05:22:21 -0800

    Pipe login to bash

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-aws ecr get-login 
+aws ecr get-login | bash
 docker build -t ${REPOSITORY_NAME} .
 docker tag ${REPOSITORY_NAME}:latest \
     ${REPOSITORY_URL}/${REPOSITORY_NAME}:latest
