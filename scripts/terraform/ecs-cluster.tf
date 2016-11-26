2ea420663bee681a5838ece0293a271573ab9195
Jim Counts <jcounts18@gmail.com>
2016-11-26 07:06:03 -0800

    Added load balancer

--- scripts/terraform/ecs-cluster.tf
+++ scripts/terraform/ecs-cluster.tf
@@ -4,6 +4,11 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+resource "aws_iam_policy" "codecamp_terraform_access" {
+  name = "terraform-demo"
+  policy = "${file("terraform-demo.json")}"
+}
+
 resource "aws_ecr_repository" "codecampci_ecr" {
   name = "codecampci"
 }
@@ -12,14 +17,44 @@ resource "aws_ecs_cluster" "codecampci_ecs" {
   name = "codecampci"
 }
 
+resource "aws_alb" "codecampci_alb" {
+  name            = "codecampci"
+  security_groups = ["sg-4d6a3134"]
+
+  subnets = ["subnet-65c23212", "subnet-c7db09a2",
+    "subnet-840d1dc2",
+  ]
+
+  tags {
+    Environment = "production"
+  }
+}
+
+resource "aws_alb_target_group" "codecamp_tg" {
+  name     = "codecampci"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = "vpc-3fe3255a"
+}
+
+resource "aws_alb_listener" "codecampci_listener" {
+  "default_action" {
+    target_group_arn = "${aws_alb_target_group.codecamp_tg.arn}"
+    type             = "forward"
+  }
+
+  load_balancer_arn = "${aws_alb.codecampci_alb.arn}"
+  port              = 80
+  protocol          = "HTTP"
+}
+
 resource "aws_ecs_service" "codecampci_service" {
   name            = "codecampci"
   cluster         = "${aws_ecs_cluster.codecampci_ecs.id}"
   task_definition = "arn:aws:ecs:us-west-2:417922976742:task-definition/codecampci:4"
   desired_count   = 2
 
-  // iam_role = "arn:aws:iam::417922976742:role/demos-ci"
-
+  iam_role = "arn:aws:iam::417922976742:role/demos-ci"
 
   // TODO: use this if terraform created the policy so that terraform can destroy them in the right order
 
@@ -27,18 +62,11 @@ resource "aws_ecs_service" "codecampci_service" {
   depends_on = [
     "aws_ecs_cluster.codecampci_ecs",
   ]
-
-  // TODO: load balancer
-
-  //  load_balancer {
-
-  //    elb_name = "${aws_elb.foo.name}"
-
-  //    container_name = "mongo"
-
-  //    container_port = 8080
-
-  //  }
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.codecamp_tg.arn}"
+    container_name   = "codecampci"
+    container_port   = 3000
+  }
 }
 
 data "template_file" "user_data" {
@@ -68,5 +96,11 @@ resource "aws_autoscaling_group" "codecamp_asg" {
   launch_configuration = "${aws_launch_configuration.codecamp_lc.name}"
   max_size             = 8
   min_size             = 1
-  availability_zones = ["us-west-2a","us-west-2b","us-west-2c"]
+  availability_zones   = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  tag {
+    propagate_at_launch = true
+    key                 = "Name"
+    value               = "${aws_ecs_cluster.codecampci_ecs.name}"
+  }
 }
